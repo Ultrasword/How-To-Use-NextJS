685ac89aa704a0e05dd53042df331e0ddbe8d7e5
Peter Zhang <ultraswordkirbypink@gmail.com>
2023-01-19 08:07:20 -0500

    cards!

--- nextjs-blog/pages/home.js
+++ nextjs-blog/pages/home.js
@@ -1,5 +1,12 @@
 import Head from 'next/head'
+import Image from 'next/image'
+
+import React from 'react';
+// reactstrap components
+import {Container, Row, Col, Card, CardBody, CardTitle, CardSubtitle, CardText, Button} from "reactstrap";
+
 import styles from '../styles/Home.module.css';
+
 
 export default function Home() {
     return (
@@ -7,9 +14,13 @@ export default function Home() {
         <Head>
             <title>Home Page</title>
             <link rel="icon" href="/favicon.ico" />
+            <link
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+            />
         </Head>
 
-        <main>
+        <div className='submaindiv'>
             <div id="navbar" className={styles.navbar}>
                 <h1>Home Page</h1>
                 <table>
@@ -23,13 +34,79 @@ export default function Home() {
                 <div>
                     <h1>Content</h1>
                     <p>Welcome to the home page! This is a page full of awesome stuff!</p>
+                    <div id="p1">
+                        <h2>Who Am I?</h2>
+                    </div>
+                    
+                    <div id="p2">
+                        <h2>What Do I Do?</h2>
+                    </div>
+
+                    <div id="p3">
+                        <h2>Projects</h2>
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <Card outline style={{width: '18rem'}}>
+                                        <img alt="icon 1" src="background.jpg"/>
+    <CardBody>
+        <CardTitle tag="h5">Card title</CardTitle>
+        <CardSubtitle className="mb-2 text-muted" tag="h6">
+            Card subtitle
+        </CardSubtitle>
+        <CardText>
+            Some quick example text to build on the card title and make up the bulk of the card‘s content.
+        </CardText>
+        <Button>
+            Explore
+        </Button>
+    </CardBody>
+                                    </Card>
+                                </Col>
+                                <Col>
+                                <Card outline style={{width: '18rem'}}>
+                                        <img alt="icon 1" src="background.jpg"/>
+    <CardBody>
+        <CardTitle tag="h5">Card title</CardTitle>
+        <CardSubtitle className="mb-2 text-muted" tag="h6">
+            Card subtitle
+        </CardSubtitle>
+        <CardText>
+            Some quick example text to build on the card title and make up the bulk of the card‘s content.
+        </CardText>
+        <Button>
+            Explore
+        </Button>
+    </CardBody>
+                                    </Card>
+                                </Col>
+                                <Col>
+                                <Card outline style={{width: '18rem'}}>
+                                        <img alt="icon 1" src="background.jpg"/>
+    <CardBody>
+        <CardTitle tag="h5">Card title</CardTitle>
+        <CardSubtitle className="mb-2 text-muted" tag="h6">
+            Card subtitle
+        </CardSubtitle>
+        <CardText>
+            Some quick example text to build on the card title and make up the bulk of the card‘s content.
+        </CardText>
+        <Button>
+            Explore
+        </Button>
+    </CardBody>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
 
                 </div>
             </div>
             <div id="endbar" className={styles.endbar}>
 
             </div>
-        </main>
+        </div>
     </div>
     )
 }
